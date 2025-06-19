fb0d6446b3587e189dafcf381f1e69f547438a74
Athalla <athallaarli@gmail.com>
2025-06-19 16:45:04 +0700

    first commit

---
+++ pages/latihan.tsx
@@ -0,0 +1,5 @@
+const LatihanPage = () => {
+  return <h1>Welcome to Latihan Page</h1>;
+};
+
+export default LatihanPage;
---
+++ pages/menulis-aksara.tsx
@@ -0,0 +1,5 @@
+const MenulisAksaraPage = () => {
+  return <h1>Welcome to Menulis Aksara Page</h1>;
+};
+
+export default MenulisAksaraPage;
